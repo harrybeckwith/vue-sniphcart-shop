d8e16aa033e57745f6cb8a68664a1d6fb0dab6c5
harrybeckwith <harryjbeckwith@gmail.com>
2021-02-01 16:27:40 +0000

    domain url added

--- pages/products/_id/index.vue
+++ pages/products/_id/index.vue
@@ -14,6 +14,7 @@
             </h4>
             <div class="mt-1 text-gray-600">{{ product.description }}</div>
           </div>
+          {{this.$route.fullPath}}
           <button
             class="snipcart-add-item mt-4 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
             :data-item-id="product.id"
